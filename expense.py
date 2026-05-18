19d34096d65b9b240f554171624f6a12d9906ef8
JerrodMcHenry <joinjerrodtoday@gmail.com>
2026-05-17 18:03:55 -0700

    Added more functions

--- expense.py
+++ expense.py
@@ -12,6 +12,30 @@ def view_expenses():
     for expense in expenses:
         print(f'${expense['amount']} - {expense['category']} - {expense['description']}')
 
-add_expense(35, 'debit', 'pay for last night')
+def calculate_total():
+    total = 0
 
-print(expenses)
+    for expense in expenses:
+        total += expense["amount"]
+
+    return total 
+
+def filter_by_category(category):
+    filtered_expenses = []
+
+    for expense in expenses:
+        if expense["category"] == category:
+            filtered_expenses.append(expense)
+    
+    return filtered_expenses
+
+add_expense(25, "food", "Chipotle")
+add_expense(80, "gas", "Chevron")
+add_expense(15, "food", "Starbucks")
+
+view_expenses()
+
+print("Total spent:", calculate_total())
+
+food_expenses = filter_by_category("food")
+print(food_expenses)
